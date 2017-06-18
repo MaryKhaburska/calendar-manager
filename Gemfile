e7c32d64d6a5be576cc931aab773ce1ce7b2c19e
mary.khaburska <mary.khaburska@gmail.com>
2017-06-18 16:25:16 +0300

    test calendar#index action

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ group :development, :test do
   gem 'rspec-rails', '~> 3.5'
   gem 'shoulda-matchers', git: 'https://github.com/thoughtbot/shoulda-matchers.git', branch: 'rails-5'
   gem 'factory_girl_rails'
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- spec/controllers/calendars_controller_spec.rb
+++ spec/controllers/calendars_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe CalendarsController, type: :controller do
+  before { @calendar = FactoryGirl.create(:calendar) }
 
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "loads all of the calendars into @calendars" do
+      get :index
+      expect(assigns(:calendars)).to match_array([@calendar])
+    end
+  end
 end

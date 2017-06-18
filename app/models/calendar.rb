73920e7ee9f1ff660202223f9c845b1cc5a8730b
mary.khaburska <mary.khaburska@gmail.com>
2017-06-18 21:44:54 +0300

    add carriervawe for upload files

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -2,6 +2,8 @@ class Calendar < ApplicationRecord
   validates :name, presence: true
   has_many :events
 
+  mount_uploader :ical_link, IcalLinkUploader
+
   # uncomment after create User
   # validates :user_id, presence: true
 end
